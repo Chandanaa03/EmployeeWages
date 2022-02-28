a886ff71b73f745c093ae207aa445a9dc8a996db
chandana03 <chandanareshma01@gmail.com>
2022-03-01 00:13:59 +0530

    adding comments

--- Program.cs
+++ Program.cs
@@ -2,8 +2,15 @@
 
 namespace Employeewages_Day3
 {
+    /// <summary>
+    /// contains main method
+    /// </summary>
     class program
     {
+        /// <summary>
+        /// Displaying Greeting message
+        /// </summary>
+        /// <param name="args"></param>
         static void Main (string[] args)
         {
             Console.WriteLine("Welcome to employee wage program");
