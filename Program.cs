7f303bb3edb4b6bde46d35e9f5bb1e679a4c99f8
chandana03 <chandanareshma01@gmail.com>
2022-03-01 00:12:02 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Employeewages_Day3
+{
+    class program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage program");
+        }
+}
+
+}
