fac35470ae7fd1c73b84d04f937bf458f801a06f
chandana03 <chandanareshma01@gmail.com>
2022-03-01 01:03:57 +0530

    employeewages

--- empwage.cs
+++ empwage.cs
@@ -11,18 +11,24 @@ namespace Employeewages_Day3
         public static void Employeewages()
         {
             int Full_Time = 1;
+            int Part_Time = 2;
             int emp_rate_per_hour = 20;
             int emphrs = 0;
             int emp_wage = 0;
 
             Random random = new Random();
-            int empcheck = random.Next(2);
+            int empcheck = random.Next(3);
 
             if (empcheck == Full_Time)
             {
                 Console.WriteLine("employee is Present");
                 emphrs = 8;
 
+            }
+            else if( empcheck == Part_Time)
+            {
+                Console.WriteLine("employee is working part time");
+                emphrs = 4;
             }
             else
             {
